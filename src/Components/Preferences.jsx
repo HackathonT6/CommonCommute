0a1bcf32a0de94ad9b38e2320d566e666fe5891e
Guy Elnatan <guyelnatan@gmail.com>
2022-01-12 23:15:26 +0200

    fixed selector bug

--- src/Components/Preferences.jsx
+++ src/Components/Preferences.jsx
@@ -5,27 +5,29 @@ import { Box } from "@mui/system";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { v4 as uuidv4 } from "uuid";
 
 const Preferences = () => {
 	const [prefGroupNum, setPrefGroupNum] = React.useState(0);
-	const [prefs, setPrefs] = React.useState({
-		Music: 3,
-		Movies: 3,
-		History: 3,
-		Politics: 3,
-		PC: 3,
-		Foreign_languages: 3,
-		Art_exhibitions: 3,
-		Religion: 3,
-		Countryside: 3,
-		outdoors: 3,
-		Dancing: 3,
-		Musical_instruments: 3,
-		Shopping: 3,
-		Science_and_technology: 3,
-		Theatre: 3,
-		Adrenaline_sports: 3,
-	});
+	const dbPrefs = {
+		Music: 0,
+		Movies: 0,
+		History: 0,
+		Politics: 0,
+		PC: 0,
+		Foreign_languages: 0,
+		Art_exhibitions: 0,
+		Religion: 0,
+		Countryside: 0,
+		outdoors: 0,
+		Dancing: 0,
+		Musical_instruments: 0,
+		Shopping: 0,
+		Science_and_technology: 0,
+		Theatre: 0,
+		Adrenaline_sports: 0,
+	}; //!!!!!!!!!! will be changed to get data from DB
+	const [prefs, setPrefs] = React.useState({});
 
 	const handleChange = (e) => {
 		setPrefs({
@@ -38,6 +40,10 @@ const Preferences = () => {
 		event.preventDefault();
 		console.log("prefs: ", prefs);
 	};
+
+	React.useEffect(() => {
+		setPrefs(dbPrefs);
+	}, []);
 
 	const customTheme = createTheme({
 		palette: {
@@ -57,7 +63,7 @@ const Preferences = () => {
 			<div className="page-wrapper">
 				<h1>Preferences</h1>
 				<div className="pref-form-container">
-					<div style={{ flexBasis: "10%" ,alignSelf:"center"}}>
+					<div style={{ flexBasis: "10%", alignSelf: "center" }}>
 						{prefGroupNum !== 0 ? (
 							<Button onClick={() => setPrefGroupNum((prev) => prev - 5)}>
 								<ArrowBackIosIcon />
@@ -71,36 +77,44 @@ const Preferences = () => {
 						style={{ flexBasis: "80% !important", padding: "0" }}
 					>
 						<form onSubmit={handleSubmit}>
-							{Object.keys(prefs)
-								.slice(prefGroupNum, prefGroupNum + 4)
-								.map((key) => {
-									return (
-										<>
-											<Typography id={`choose-pref=${key}`} gutterBottom>
-												{key.replace(/_/g, " ")}
-											</Typography>
-											<Slider
-												aria-label={key.replace(/_/g, " ")}
-												getAriaValueText={prefs.key}
-												valueLabelDisplay="auto"
-												step={1}
-												marks={[
-													{ value: 1, label: "1" },
-													{ value: 2, label: "2" },
-													{ value: 3, label: "3" },
-													{ value: 4, label: "4" },
-													{ value: 5, label: "5" },
-												]}
-												min={1}
-												max={5}
-												name={key}
-												value={prefs.key}
-												onChange={handleChange}
-												color="secondary"
-											/>
-										</>
-									);
-								})}
+							{prefs &&
+								Object.keys(prefs)
+									.slice(prefGroupNum, prefGroupNum + 4)
+									.map((key) => {
+										return (
+											<>
+												<Typography
+													key={uuidv4()}
+													id={`choose-pref=${key}`}
+													gutterBottom
+												>
+													{key.replace(/_/g, " ")}
+												</Typography>
+												<Slider
+													key={uuidv4()}
+													aria-label={key.replace(/_/g, " ")}
+													// getAriaValueText={prefs[key]}
+													placeholder={prefs[key]}
+													valueLabelDisplay="auto"
+													defaultValue={prefs[key]}
+													step={1}
+													marks={[
+														{ value: 1, label: "1" },
+														{ value: 2, label: "2" },
+														{ value: 3, label: "3" },
+														{ value: 4, label: "4" },
+														{ value: 5, label: "5" },
+													]}
+													min={1}
+													max={5}
+													name={key}
+													value={prefs[key]}
+													onChange={handleChange}
+													color="secondary"
+												/>
+											</>
+										);
+									})}
 
 							<Button
 								id="appButton"
@@ -113,7 +127,7 @@ const Preferences = () => {
 							</Button>
 						</form>
 					</Box>
-					<div style={{ flexBasis: "10%" ,alignSelf:"center"}}>
+					<div style={{ flexBasis: "10%", alignSelf: "center" }}>
 						{prefGroupNum < 10 ? (
 							<Button onClick={() => setPrefGroupNum((prev) => prev + 5)}>
 								<ArrowForwardIosIcon />
@@ -129,3 +143,7 @@ const Preferences = () => {
 };
 
 export default Preferences;
+
+
+//backend key.replace
+//setprefs route
